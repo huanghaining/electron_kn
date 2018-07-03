201d55f72378c626959086697d62d61eabe4cdec
huanghaining <15817345963@163.com>
2018-07-03 09:22:11 +0800

    修改commonjs文档

--- src/main/common.js
+++ src/main/common.js
@@ -1,19 +1,34 @@
-var x=document.getElementById("main");
+var x=document.getElementById("textedit");
 
 function getLocation()
 {
+    window.alert('error')
+    console.log('getLocation enter')
     if (navigator.geolocation)
     {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        window.alert('error111')
+        navigator.geolocation.getCurrentPosition(showPosition,errorCallBack);
     }
     else
     {
-        x.innerHTML="该浏览器不支持获取地理位置。";
+        window.alert('error22')
+        x.value="该浏览器不支持获取地理位置。";
     }
+}
+
+function errorCallBack(params) {
+    window.alert('get position error')  
 }
 
 function showPosition(position)
 {
-    x.innerHTML="纬度: " + position.coords.latitude + 
-    "<br>经度: " + position.coords.longitude;    
+    window.alert('get position success') 
+    value="纬度: " + position.coords.latitude + 
+    "<br>经度: " + position.coords.longitude; 
+    window.alert(value)   
+}
+
+function getName() {
+document.getElementById('textedit').value= 'hello world'
+
 }
